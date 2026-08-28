6359ba7881a9f3040a892115b2cd0535cbf1f0c1
Arye Levin <arye.l@freetail.com>
2026-08-28 12:28:12 +0300

    Some extra logging for better understanding

--- src/entity.ts
+++ src/entity.ts
@@ -2471,7 +2471,9 @@ export class ZigbeeDevice extends ZigbeeEntity {
           'fanMode',
           (newValue: FanControl.FanMode, oldValue: FanControl.FanMode, context) => {
             if (newValue === oldValue) return;
-            zigbeeDevice.log.info(`Fan mode changed from ${fanModeLookup[oldValue]} to ${fanModeLookup[newValue]} context: ${context.fabric === undefined ? 'offline' : 'online'}`);
+            zigbeeDevice.log.info(
+              `Fan mode changed from ${fanModeLookup[oldValue]} (${oldValue}) to ${fanModeLookup[newValue]} (${newValue}) context: ${context.fabric === undefined ? 'offline' : 'online'}`,
+            );
             // if (context.fabric === undefined) return; // Do not set publish when offline (offline means that the change happened not from matter side)
             if (context.fabric !== undefined && zigbeeDevice.propertyMap.has('fan_mode' + endpointSuffix) && newValue !== FanControl.FanMode.Off)
               zigbeeDevice.publishCommand('FanMode', device.friendly_name, { ['fan_mode' + endpointSuffix]: ['off', 'low', 'medium', 'high', 'on', 'auto'][newValue] || 'auto' });
